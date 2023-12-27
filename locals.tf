f1183857d76ee94589f935e247eb539c37d217ba
Thangavelu@ <chakraodba@gmail.com>
2023-12-27 11:52:08 +0530

    this is going to be building block of my deployment

--- locals.tf
+++ locals.tf
@@ -1,16 +1,18 @@
+# locals.tf
+
 locals {
-  cloud_config_config = <<-END
-    #cloud-config
-    ${jsonencode({
-  write_files = [
-    {
-      path        = "user_data_script.sh"
-      permissions = "0755"
-      owner       = "root:root"
-      encoding    = "b64"
-      content     = filebase64("${path.module}/user_data_script.sh")
-    },
-  ]
-})}
-  END
+  # Common values that can be reused across the configuration
+
+  # Example: AWS region
+  aws_region = "ap-south-1"
+
+  # Example: Define a map of tags to assign to resources
+  common_tags = {
+    Environment = "Production"
+    Department  = "IT"
+    Owner       = "Chakravarthi Thangavelu"
+    Name        = "django-server-lu-1-dev"
+    # Add more tags as needed
+  }
+
 }
--- main.tf
+++ main.tf
@@ -1,35 +1,37 @@
 #adding this comment
 terraform {
   backend "s3" {
-    bucket  = "temp1983"
-    key     = "django/statefile.tfstate"
-    region  = "ap-south-1"
-    encrypt = true
+    bucket  = var.bucket_name
+    #key     = "django/statefile.tfstate"
+    key     = var.bucket_key
+    region  = local.aws_region
+    encrypt = var.need_encrypt
   }
 }
 
 provider "aws" {
-  region = "ap-south-1"
+  region = local.aws_region
   # Add any other necessary authentication parameters
 }
 
 # Reference to the existing VPC by ID
 data "aws_vpc" "existing_vpc" {
-  id = "vpc-028b13bb3e33cf1ae" # Replace with your existing VPC ID
+  id = var.vpc_id # Replace with your existing VPC ID
 }
 
 # Reference to the existing subnet within the existing VPC by ID
 data "aws_subnet" "existing_subnet" {
   vpc_id = data.aws_vpc.existing_vpc.id
-  id     = "subnet-0d88dc6bc2c2c2b27" # Replace with your existing subnet ID
+  id     = var.subnet_id # Replace with your existing subnet ID
 }
 
 resource "aws_instance" "example_instance" {
-  ami                         = "ami-03f4878755434977f"
-  instance_type               = "t2.small"
-  key_name                    = "django-server-l-0-dev-key"
+  ami                         = var.ec_ami
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
   subnet_id                   = data.aws_subnet.existing_subnet.id
-  associate_public_ip_address = false # Associates a public IP address with the instance
+  associate_public_ip_address = var.pub_ip_associate # Associates a public IP address with the instance
+  tags = local.common_tags
 
 }
 
